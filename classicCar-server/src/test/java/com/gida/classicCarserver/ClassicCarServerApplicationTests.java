fe84fd48bfd34368d7cb5a38324d6e166f3f3e72
Kevin Gida <kevingida@gmail.com>
2023-08-09 13:21:06 +0200

    Initial data

---
+++ classicCar-server/src/test/java/com/gida/classicCarserver/ClassicCarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gida.classicCarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicCarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
